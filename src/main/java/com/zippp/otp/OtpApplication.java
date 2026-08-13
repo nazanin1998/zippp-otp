dca2d9c55252f156ac6ed56cecac99379c7d1315
nazanin feredyooni <nazanin.fereydoonizade@gmail.com>
2026-08-13 21:20:54 +0330

    fix: make rabbit config dynamic

--- src/main/java/com/zippp/otp/OtpApplication.java
+++ src/main/java/com/zippp/otp/OtpApplication.java
@@ -1,12 +1,13 @@
 package com.zippp.otp;
 
+import com.zippp.otp.config.properties.OtpProperties;
 import com.zippp.otp.config.properties.RabbitOtpProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({RabbitOtpProperties.class})
+@EnableConfigurationProperties({RabbitOtpProperties.class, OtpProperties.class})
 public class OtpApplication {
 
     public static void main(String[] args) {
