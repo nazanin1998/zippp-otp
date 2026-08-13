8f3581ee3d5c7b4e58b21ded661f3aa22cf59ef7
nazanin feredyooni <nazanin.fereydoonizade@gmail.com>
2026-08-13 15:44:40 +0330

    fix: add enable configuration

--- src/main/java/com/zippp/otp/OtpApplication.java
+++ src/main/java/com/zippp/otp/OtpApplication.java
@@ -1,9 +1,12 @@
 package com.zippp.otp;
 
+import com.zippp.otp.config.properties.RabbitOtpProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({RabbitOtpProperties.class})
 public class OtpApplication {
 
     public static void main(String[] args) {
